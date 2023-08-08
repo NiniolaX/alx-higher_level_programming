247aabe4807a85470cb3cc8475a3326f5fe9a135
NiniolaX <loveafinni16@gmail.com>
2023-08-08 03:54:18 -0700

    Updated script to handle wrong type

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-number = 98
-print(f"{int(number)} Battery street")
+number = 'Holberton'
+print(f"{int(number)} Battery street") if isinstance(number, int) else print("ValueError: Unknown format code 'd' for object of type 'str'")
