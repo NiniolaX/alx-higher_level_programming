b070e45c728de53092cb2f50739996bf7ad1fb17
NiniolaX <loveafinni16@gmail.com>
2023-08-08 04:27:30 -0700

    Handled wrong type correctly using format specifier

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 number = 98
-print(f"{int(number)} Battery street") if isinstance(number, int) else print("ValueError: Unknown format code 'd' for object of type 'str'")
+print(f"{number:d} Battery street") 
