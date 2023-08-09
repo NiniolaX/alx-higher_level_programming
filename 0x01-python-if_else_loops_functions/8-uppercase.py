a9d3c5d9895ebfb28dff00c5472d3e0a88c4b897
NiniolaX <loveafinni16@gmail.com>
2023-08-08 20:40:47 -0700

    Handled case where empty string is passed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def uppercase(str):
+    if str == "":
+        print('{}'.format('\n'))
+        return
+
     i = 0
     while i < len(str):
         if str[i] >= 'a' and str[i] <= 'z':
-            u = ord(str[i]) - 32
-            print('{}'.format(chr(u)), end='' if i + 1 != len(str) else '\n')
-        else:
-            print('{}'.format(str[i]), end='' if i + 1 != len(str) else '\n')
+            upper = chr(ord(str[i]) - 32)
+        print('{}'.format(upper if 'a' <= str[i] <= 'z' else str[i]),
+              end='' if i + 1 != len(str) else '\n')
         i += 1
