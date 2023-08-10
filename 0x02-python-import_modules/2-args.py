f724f2a1a456991743549d76493ff7b89f8abb23
NiniolaX <loveafinni16@gmail.com>
2023-08-10 15:20:22 -0700

    Fixed pycodestyle warnings

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     ac = len(sys.argv)
     print('{} {}{}'.format(ac - 1, 'argument' if ac - 1 == 1 else 'arguments',
-           '.' if ac - 1 == 0 else ':'))
-    for a in range (1, ac):
+                           '.' if ac - 1 == 0 else ':'))
+    for a in range(1, ac):
         print('{}: {}'.format(a, sys.argv[a]))
         a += 1
