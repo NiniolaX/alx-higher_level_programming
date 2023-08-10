c7f7fdd91319bf43626d26e11722987e1b43e634
NiniolaX <loveafinni16@gmail.com>
2023-08-10 15:25:47 -0700

    Used more meaningful variables

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     ac = len(sys.argv)
     print('{} {}{}'.format(ac - 1, 'argument' if ac - 1 == 1 else 'arguments',
                            '.' if ac - 1 == 0 else ':'))
-    for a in range(1, ac):
-        print('{}: {}'.format(a, sys.argv[a]))
-        a += 1
+    for i in range(1, ac):
+        print('{}: {}'.format(i, sys.argv[i]))
+        i += 1
