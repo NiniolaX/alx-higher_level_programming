925a8afb05275b987fc7e9ad06e9f1e00de0fe6c
NiniolaX <loveafinni16@gmail.com>
2023-08-11 12:10:09 -0700

    Adjusted code to not run when imported

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
-hidden_names = dir(hidden_4)
-for name in hidden_names:
-    if not name.startswith('__'):
-        print('{}'.format(name), end='\n')
+if __name__ == "__main__":
+    hidden_names = dir(hidden_4)
+    for name in hidden_names:
+        if not name.startswith('__'):
+            print('{}'.format(name), end='\n')
