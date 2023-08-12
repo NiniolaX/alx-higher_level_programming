0234b2b44d19656ff0f6c7203865549eb1a9c240
NiniolaX <loveafinni16@gmail.com>
2023-08-12 14:25:20 -0700

    Made corrections for incorrect output on multiple negative number inserts

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,43 +1,45 @@
 #include "lists.h"
 
 /**
- * insert_node - Inserts a number into a sorted linked list
- * @head: Address of head node of list
- * @number: Number to be inserted
- * Return: Address of new node on success, NULL on fail
+ * insert_node - Inserts a node into a singly linked list
+ * @head: Address of head node
+ * @number: Interger element of new node
+ * Return: New node on sucess, NULL on fail
  */
 listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *ptr, *new;
 
-	/* Check that valid address is passed */
 	if (head == NULL)
 		return (NULL);
 	ptr = *head;
 
-	/* Build new node */
+	/* Build the new node */
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
 	new->next = NULL;
 
-	/* Add new node if list is empty */
+	/* If list is empty*/
 	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
 
-	/* If list is not empty */
-	if (number < 0 && ptr->n >= 0)
+	/* If number is negative */
+	if (number < 0 && number < (*head)->n)
 	{
 		new->next = *head;
 		*head = new;
 		return (new);
 	}
-	while (ptr->next != NULL && number > (ptr->next)->n)
+
+	/* If list is not empty */
+	while (ptr->next != NULL && (ptr->next)->n < number)
 		ptr = ptr->next;
+
 	new->next = ptr->next;
 	ptr->next = new;
 
