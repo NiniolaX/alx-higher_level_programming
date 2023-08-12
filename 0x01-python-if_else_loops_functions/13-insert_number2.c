0b668febce8396255f33e5c21c2b8d5c1c675b63
NiniolaX <loveafinni16@gmail.com>
2023-08-12 13:33:19 -0700

    Test

---
+++ 0x01-python-if_else_loops_functions/13-insert_number2.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a node into a singly linked list
+ * @head: Address of head node
+ * @number: Interger element of new node
+ * Return: New node on sucess, NULL on fail
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *ptr, *new;
+
+	if (head == NULL)
+		return (NULL);
+	ptr = *head;
+
+	/* Build the new node */
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	new->next = NULL;
+
+	/* If list is empty*/
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	/* If list is not empty */
+	while (ptr != NULL && (ptr->next)->n < number)
+		ptr = ptr->next;
+	new->next = ptr->next;
+	ptr->next = new;
+
+	return (new);
+}
