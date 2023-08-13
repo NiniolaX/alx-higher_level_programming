7ac4814df1bbba33130e58e34de6e029d9576147
NiniolaX <loveafinni16@gmail.com>
2023-08-13 06:57:17 -0700

    Reverted last commit

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    rev_list = my_list.copy()
-    rev_list.reverse()
-    for num in rev_list:
+    my_list.reverse()
+    for num in my_list:
          print('{:d}'.format(num))
