d4e74626f49987f63154d5c81ed7947777dd789e
NiniolaX <loveafinni16@gmail.com>
2023-08-13 06:06:50 -0700

    Improved readability of code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for num in row:
-            print('{:d}'.format(num), end='')
             if row.index(num) < len(row) - 1:
-                print(' ', end='')
+                print('{:d}'.format(num), end=' ')
+            else:
+                print('{:d}'.format(num), end='')
         print()
