3ba4e54ea2523dc1ce4d0eaa5f3bb53bfd5e4211
NiniolaX <loveafinni16@gmail.com>
2023-08-13 13:48:09 -0700

    Wrote function to find maximum of a list of integers

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    list_len = len(my_list)
+
+    # If list is empty
+    if list_len == 0:
+        return None
+
+    max_num = my_list[0]
+
+    for i in range(list_len):
+        if my_list[i] > max_num:
+            max_num = my_list[i]
+        i += 1
+
+    return max_num
