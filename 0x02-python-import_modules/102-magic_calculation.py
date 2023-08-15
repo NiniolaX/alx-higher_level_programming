f10cee025a70cbfe35df4026ef07fe9b49c9be13
NiniolaX <loveafinni16@gmail.com>
2023-08-15 12:50:25 -0700

    Moved import inside function definition

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-from magic_calculation_102 import add, sub
-
-
 def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
 
     add = magic_calculation_102.add
     sub = magic_calculation_102.sub
 
     if a < b:
-        c = add(b, a)
+        c = add(a, b)
         for i in range(4, 90, 6):
             c = add(c, i)
         return c
     else:
         return sub(b, a)
+
+    return 0
