07765d9776dcf4bd01fb9ff96acdf17ab217010c
NiniolaX <loveafinni16@gmail.com>
2023-08-16 19:31:04 -0700

    Hamdled case of multiple occurences of element to replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    i = new_list.index(search)
-    new_list[i] = replace
+    for i, elem in enumerate(new_list):
+        if elem == search:
+            new_list[i] = replace
     return new_list
