84778be56325c443189c185411ea130f8ca42502
NiniolaX <loveafinni16@gmail.com>
2023-08-16 18:51:13 -0700

    Wrote function to square a nested matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    if matrix:
+        for i in range(3):
+            new_matrix.append(list(map(lambda x: x ** 2, matrix[i])))
+        return new_matrix
