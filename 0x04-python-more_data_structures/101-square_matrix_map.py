0aff47d2038d776b45c9b03e7ab3fe112e840f33
NiniolaX <loveafinni16@gmail.com>
2023-08-19 10:21:41 -0700

    Used map instead of loop to build new matrix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda x: x ** 2, elem)) for elem in matrix]
+    return list(map(lambda ls: list(map(lambda x: x ** 2, ls)), matrix))
