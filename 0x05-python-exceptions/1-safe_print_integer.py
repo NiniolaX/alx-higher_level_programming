1ad4bd486372742f6dcdd94e461a96b4b3bc62b7
NiniolaX <loveafinni16@gmail.com>
2023-08-29 09:12:12 -0700

    Optimized code

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,9 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
-        return False
-    except TypeError:
+    except (TypeError, ValueError):
         return False
     else:
         return True
