85060cd9955ca7a8dddb9b21cf90c0bfb0e0b06e
NiniolaX <loveafinni16@gmail.com>
2023-08-29 09:53:39 -0700

    Optimized function

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             i = i + 1
     except IndexError:
         pass
-    finally:
-        print()
-        return item_count
+
+    print()
+    return item_count
