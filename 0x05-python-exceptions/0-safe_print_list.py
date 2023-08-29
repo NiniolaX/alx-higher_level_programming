d4643dc75a68ca3d80de3351b13c8d171919d424
NiniolaX <loveafinni16@gmail.com>
2023-08-29 06:53:21 -0700

    Revert "Optimized function"
    This reverts commit 6389389df8ec05295a622c84f48a4680cf4738d4.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     try:
         item_count = 0
-        for item in my_list:
-            print(item, end="")
+        for i in range(x):
+            print(my_list[i], end="")
             item_count = item_count + 1
+            i = i + 1
     except IndexError:
         pass
     finally:
