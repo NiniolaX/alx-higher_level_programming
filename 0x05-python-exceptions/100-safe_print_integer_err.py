5db33b6589ce2d600884e130b013668d4aa04cd5
NiniolaX <loveafinni16@gmail.com>
2023-08-30 14:29:27 -0700

    Removed redundant code

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,11 +4,9 @@ import sys
 
 
 def safe_print_integer_err(value):
-    return_val = True
     try:
         print("{:d}".format(value))
+        return True
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
-        return_val = False
-    finally:
-        return return_val
+        return False
