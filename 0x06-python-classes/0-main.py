9f592c131f7bd30ebd6760341f7206bcfff5d982
NiniolaX <loveafinni16@gmail.com>
2023-09-02 07:18:42 -0700

    Added documentation for module

--- 0x06-python-classes/0-main.py
+++ 0x06-python-classes/0-main.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+
+"""
+This script demonstrates the use of the Square class in module '0-square'
+module.
+"""
+
+# Import the module
 Square = __import__('0-square').Square
 
+# Create an instance of the Square class
 my_square = Square()
+
+# Prints the type of the my_square object
 print(type(my_square))
+
+# Prints the dictionary representation of 'my_square.
 print(my_square.__dict__)
