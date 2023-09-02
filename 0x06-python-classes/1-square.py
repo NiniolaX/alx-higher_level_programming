e6e29558b41db9e9f614542fc3854c801a8d8635
NiniolaX <loveafinni16@gmail.com>
2023-09-02 07:58:30 -0700

    Wrote module that defines a class and its size as a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class for representing squares.
+
+This module provides a basic representation of a square and its size.
+
+Class:
+    Square: A basic representation of a square.
+
+Attributes:
+    None.
+
+Functions:
+    None.
+"""
+
+
+class Square:
+    """A class that defines a square.
+
+    This class is a representation of a square.
+
+    Attributes:
+        size (int): The size of the square as a positive integer.
+
+    Methods:
+        None.
+    """
+
+    def __init__(self, size):
+        self.__size = size
