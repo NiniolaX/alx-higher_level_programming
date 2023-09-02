ea499494748525a6e6b558d8b72a24fc9986201e
NiniolaX <loveafinni16@gmail.com>
2023-09-02 07:10:01 -0700

    Wrote an empty class- Square, that defines a square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class Square:
+    """An empty class that defines a square.
+
+    This class is a basic representation of a square. It does not include any
+    attributes or methods. It serves a starting point for building more complex
+    square-related classes.
+
+    Attributes:
+        None.
+
+    Methods:
+        None.
+    """
+
+    pass
