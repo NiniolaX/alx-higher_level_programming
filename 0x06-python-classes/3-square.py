03902097619038defedb9b85ab281160d6328827
NiniolaX <loveafinni16@gmail.com>
2023-09-02 09:04:30 -0700

    Included method to calculate area of square in Square class

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class for representing squares.
+
+This module provides a basic representation of a square and its size.
+
+Class:
+    Square: A basic representation of a square.
+
+Attributes:
+    None.
+
+Functions:
+    None.
+"""
+
+
+class Square:
+    """A class that defines a square.
+
+    This class is a representation of a square and provides methods for
+    calculating its area.
+
+    Attributes:
+        size (int): The size of the square as a positive integer. Defaults
+        to 0.
+
+    Methods:
+        area():
+            Calculates and returns the area of an instance of the Square.
+            Args:
+                None.
+            Returns:
+                int: The area of the square.
+
+    Raises:
+        TypeError: If 'size' is not an integer.
+        ValueError: If 'size' is less than 0.
+    """
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Returns the area of the square."""
+        return self.__size ** 2
