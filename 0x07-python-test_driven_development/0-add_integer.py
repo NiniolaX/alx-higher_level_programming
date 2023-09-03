190a98ec15643e6e41d9e5f8d4c59800b326d2b3
NiniolaX <loveafinni16@gmail.com>
2023-09-03 12:20:46 -0700

    Wrote function to add two integers including docstring test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+This module defines a function add_integer for adding two integer or floats.
+
+Attributes:
+    None
+
+Functions:
+    add_integer: Adds two numbers.
+"""
+
+
+def add_integer(a, b=98):
+    """Addition function
+
+    Function adds two numbers.
+
+    Args:
+        a (int or float): The first integer.
+        b (int or float): The second integer.
+
+    Returns:
+        int or float: The sum of a and b
+
+    Raises:
+        TypeError: If a or b are not integers or floats.
+    """
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
