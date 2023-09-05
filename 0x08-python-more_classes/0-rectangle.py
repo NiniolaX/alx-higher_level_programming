b21d4a9a9d0f0d873c17a33047d879c8585bb7f4
NiniolaX <loveafinni16@gmail.com>
2023-09-04 18:32:38 -0700

    Created readme, Wrote empty class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class 'Rectangle'.
+
+Class:
+    Rectangle: A basic representation of a rectangle with no atttributes or
+    methods.
+
+Attributes:
+    None.
+
+Functions:
+    None.
+"""
+
+
+class Rectangle:
+    """An empty class that defines a rectangle.
+
+    This class is a basic representation of a rectangle. It serves as a
+    starting point for building more complex Rectangle-related classes.
+
+    Attributes:
+        None.
+
+    Methods:
+        None.
+    """
+    pass
