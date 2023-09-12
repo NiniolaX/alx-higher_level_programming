ca8dc42b3780089fb78470e0945a368518b9c824
NiniolaX <loveafinni16@gmail.com>
2023-09-12 20:19:56 +0100

    Solution to task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This module defines a function that returns the list of available attributes
+and methods of an object.
+
+Attributes:
+    None
+
+Functions:
+    lookup - Returns the list of available attributes and methods of an onject.
+"""
+
+
+def lookup(obj=None):
+    """Returns list of available attributes and methods of an object
+    
+    Args:
+        obj (object): Object to lookup
+    
+    Returns:
+        list: Available attributes and methods of the object
+    """
+    return dict(obj)
