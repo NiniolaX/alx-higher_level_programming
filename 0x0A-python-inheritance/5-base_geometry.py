bf1ae2e13fac47f344357c0933143f16b4be38db
NiniolaX <loveafinni16@gmail.com>
2023-09-13 06:28:06 +0100

    Solution to Task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+This module defines an empty class 'BaseGeometry'.
+
+Attributes:
+    None
+
+Classes:
+    BaseGeometry: An empty class.
+"""
+
+
+class BaseGemetry:
+    """An empty class"""
