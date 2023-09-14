9b275ccc33303d6ef49ed764f0a665eec0e7c085
NiniolaX <loveafinni16@gmail.com>
2023-09-14 03:40:48 -0700

    Modified function to return False if object is same class as specified class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -26,4 +26,7 @@ def inherits_from(obj, a_class):
         bool: True if object is an instance of a class that inherits from the
         specified class; otherwise, False.
     """
-    return issubclass(type(obj), a_class)
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
