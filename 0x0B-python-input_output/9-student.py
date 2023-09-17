162832ba99bb77979746dcb41433f7c920e92809
NiniolaX <loveafinni16@gmail.com>
2023-09-17 10:17:47 +0100

    Wrote a class representing a student with a method to return a dictionary representation of the class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module defines a class representing a Student.
+
+Class:
+    Student: Representation of a student.
+
+Attributes:
+    first_name: First name of student.
+    last_name: Last name of student.
+    age: Age of student.
+"""
+
+
+class Student:
+    """A class that defines a student.
+
+    This class is a basic representation of a student.
+
+    Attributes:
+        first_name: First name of student.
+        last_name: Last name of student.
+        age: Age of student.
+
+    Methods:
+        to_json: Retrieves a dictionary representation of an instance of
+                the class.
+    """
+    def __init__(self, first_name="", last_name="", age=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        return self.__dict__
