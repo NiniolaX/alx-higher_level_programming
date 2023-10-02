5a680b982b33749afbdbc0b011c2ff766aee8c9d
NiniolaX <loveafinni16@gmail.com>
2023-10-02 02:12:03 +0100

    Updated unittest for Rectangle class

--- 0x0C-python-almost_a_circle/tests/rectangle_test.py
+++ 0x0C-python-almost_a_circle/tests/rectangle_test.py
@@ -28,7 +28,103 @@ class TestRectangleClass(unittest.TestCase):
     def test_constructor_with_id(self):
         """Tests constructor of Rectangle class with id"""
         rect = Rectangle(5, 7, id=15)
-        self.assertEqual(rect.id, 12)
+        self.assertEqual(rect.id, 15)
+
+    def test_width_getter(self):
+        """Tests width getter"""
+        rect = Rectangle(3, 8, x=1, y=2)
+        self.assertEqual(rect.width, 3)
+
+    def test_width_type_error(self):
+        """Test validity of width type"""
+        rect = Rectangle(3, 8, x=1, y=2)
+        with self.assertRaises(TypeError):
+            rect.width = 'a'
+        with self.assertRaises(TypeError):
+            rect.width = True
+        with self.assertRaises(TypeError):
+            rect.width = [1,2,3]
+        with self.assertRaises(TypeError):
+            rect.width = (1,2,3)
+        with self.assertRaises(TypeError):
+            rect.width = {1: 'a', 2: 'b'}
+
+    def test_width_value_error(self):
+        """Test validity of width value"""
+        rect = Rectangle(5, 4, x=1, y=2)
+        with self.assertRaises(ValueError):
+            rect.width = -1
+        with self.assertRaises(ValueError):
+            rect.width = 0
+
+    def test_height_getter(self):
+        """Tests height getter"""
+        rect = Rectangle(3, 8, x=1, y=2)
+        self.assertEqual(rect.height, 8)
+
+    def test_height_type_error(self):
+        """Test validity of width type"""
+        rect = Rectangle(3, 8, x=1, y=2)
+        with self.assertRaises(TypeError):
+            rect.height = 'b'
+        with self.assertRaises(TypeError):
+            rect.width = False
+        with self.assertRaises(TypeError):
+            rect.height = [1,2,3]
+        with self.assertRaises(TypeError):
+            rect.height = (1,2,3)
+        with self.assertRaises(TypeError):
+            rect.height = {1: 'a', 2: 'b'}
+
+    def test_height_value_error(self):
+        """Test validity of width value"""
+        rect = Rectangle(5, 4, x=1, y=2)
+        with self.assertRaises(ValueError):
+            rect.height = -100
+        with self.assertRaises(ValueError):
+            rect.height = 0
+
+    def test_x_getter(self):
+        """Tests x getter"""
+        rect = Rectangle(3, 8, x=1, y=2)
+        self.assertEqual(rect.x, 1)
+
+    def test_x_type_error(self):
+        """Test validity of x type"""
+        rect = Rectangle(3, 8, x=1, y=2)
+        with self.assertRaises(TypeError):
+            rect.x = 'b'
+        with self.assertRaises(TypeError):
+            rect.x = False
+        with self.assertRaises(TypeError):
+            rect.x = [1,2,3]
+
+    def test_x_value_error(self):
+        """Test validity of x value"""
+        rect = Rectangle(5, 4, x=1, y=2)
+        with self.assertRaises(ValueError):
+            rect.x = -1000
+
+    def test_y_getter(self):
+        """Tests y getter"""
+        rect = Rectangle(3, 8, x=1, y=2)
+        self.assertEqual(rect.y, 2)
+
+    def test_y_type_error(self):
+        """Test validity of y type"""
+        rect = Rectangle(3, 8, x=1, y=2)
+        with self.assertRaises(TypeError):
+            rect.y = 'b'
+        with self.assertRaises(TypeError):
+            rect.y = True
+        with self.assertRaises(TypeError):
+            rect.y = {1,2,3}
+
+    def test_y_value_error(self):
+        """Test validity of y value"""
+        rect = Rectangle(5, 4, x=1, y=2)
+        with self.assertRaises(ValueError):
+            rect.y = -10000
 
 if __name__ == "__main__":
     unittest.main()
