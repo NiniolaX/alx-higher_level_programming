b64eecd09b44529cef93719759510b6dc784c0c7
NiniolaX <loveafinni16@gmail.com>
2023-12-14 00:15:12 +0100

    Wrote scripts for tasks 7 and 8 (7) Inserts a new row in table first_table in database hbtn (8) Displays the number of records with id=89 in first_table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Script inserts a new row in a table
+-- Insert a new row in table 'first_table' in database hbtn_0c_0 d
+INSERT INTO first_table (id, name) VALUES (89, 'Best School')
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- Script that displays the number of records with a specified attribute
+-- Display number of records with id=89 in table 'first_table'
+SELECT COUNT(*)
+	FROM first_table
+		WHERE id = 89
