424e18a655613479b9b9ad6217bffc4bf33d92ad
NiniolaX <loveafinni16@gmail.com>
2024-01-31 15:07:09 +0100

    Wrote script that creates the MySQL server user user_0d_1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Script creates a new user if user doesn't exist and sets its password
+-- Create a new user 'user_0d_1' and sets its password to 'user_0d_1_pwd'
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
