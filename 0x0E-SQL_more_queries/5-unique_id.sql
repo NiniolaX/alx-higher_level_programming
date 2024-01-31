6d7fc8c67ce1458a372db5690984ed63e3ea9875
NiniolaX <loveafinni16@gmail.com>
2024-01-31 16:33:46 +0100

    Wrote script to creates a table with unique attribute and default value.

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- Creates a table with a unique attribute
+-- Create a table unique_id with a unique attribute
+CREATE TABLE IF NOT EXISTS unique_id(
+	id INT DEFAULT 1 UNIQUE,
+	name VARCHAR(256)
+	)
