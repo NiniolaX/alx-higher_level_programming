6ff4dbdfab533540128ca35b5e65dc1d00573d32
NiniolaX <loveafinni16@gmail.com>
2024-01-31 15:21:33 +0100

    Wrote script that creates a new database and user on server and grants user SELECT privilege to database

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Script creates a new user if user doesn't exist and sets its password
 -- Create a new user 'user_0d_1' and sets its password to 'user_0d_1_pwd'
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1_pwd' WITH GRANT OPTION;
