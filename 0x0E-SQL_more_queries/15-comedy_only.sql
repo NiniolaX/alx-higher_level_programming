8f98256ebfa39248d90ab2a93a2f4c95ac6a0923
NiniolaX <loveafinni16@gmail.com>
2024-02-02 00:49:41 +0100

    Wrote script which lists all the Comedy shows in the hbtn-0d_tvshows database

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- Script lists all Comedy shows in the database hbtn_0d_tvshows
+-- List all Comedy shows in the specified database
+SELECT title FROM tv_shows
+	INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+	INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+	WHERE tv_genres.name = 'Comedy'
+	ORDER BY title ASC;
