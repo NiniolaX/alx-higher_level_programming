ad54a5dc7411c6e90a559b21a5f7e3f8bc0716b1
NiniolaX <loveafinni16@gmail.com>
2024-02-02 00:29:16 +0100

    Wrote script that lists all the genres of the tv show, Dexter, in the specified database

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- Script lists all the genres of tv show 'Dexter' in a given database
+-- Lists all the genres of tv show 'Dexter' in the hbtn_0d_tvshows database
+SELECT name FROM tv_genres
+	INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+	INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title = 'Dexter'
+	ORDER BY tv_genres.name ASC;
