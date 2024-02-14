4c291554dcad36b3bd50671202cef1bf19d7e332
NiniolaX <loveafinni16@gmail.com>
2024-02-14 08:49:47 +0100

    Wrote script that prints the first argument passed to it

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const args = process.argv.slice(2); // Since first two arguments are 'node' and file path
+if (args.length === 0) {
+  console.log('No argument');
+} else {
+  console.log(args[0]);
+}
