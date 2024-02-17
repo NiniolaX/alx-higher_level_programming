850c77a1a701c2384dd865de45d6f8c45f4465c0
NiniolaX <loveafinni16@gmail.com>
2024-02-17 16:03:42 +0100

    Made file executable

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ import sys
 
 if __name__ == "__main__":
     # Create a connection with the mysql server
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          password=sys.argv[2], database=sys.argv[3])
 
     # Create a cursor object
