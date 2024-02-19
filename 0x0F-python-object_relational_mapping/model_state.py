f86b398b23db51421d902e9dd132b4b804a53563
NiniolaX <loveafinni16@gmail.com>
2024-02-19 17:36:58 +0100

    Wrote python file that contains the class definition of a State which links to a MySQL table states

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This file contains the class definition of a State which links to the MySQL
+database states.
+
+Classes:
+    Base: An instance of SQLAlchemy declarative base from which other classes
+          inherit
+    State: Class which inherits from Base and links to the 'states' table of a
+          MySQL database
+
+Functions:
+    None
+
+Attributes:
+    None
+"""
+
+from sqlalchemy import Integer, String, Column
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Links to the states table of a MySQL database.
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
