2b64a489e16ad360ac66422c9ea21b8a518b620a
NiniolaX <loveafinni16@gmail.com>
2024-04-02 18:04:42 +0100

    Reduced number of lines to 3

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to it and displays size of response body
-url=$1
-curl -s -o /dev/null -w '%{size_download}\n' "$url"
+curl -s -o /dev/null -w '%{size_download}\n' "$1"
