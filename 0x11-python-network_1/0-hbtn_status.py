815e0dbad8a249e421e79b0f62144a78312c9dcb
NiniolaX <loveafinni16@gmail.com>
2024-04-08 18:31:34 +0100

    Added script that sends a request to a URL and prints a formatted response body

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Script fetches a URL
+
+if __name__ == '__main__':
+    import urllib.request
+
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(url) as response:
+        page = response.read()
+        print('Body response:')
+        print(f"\t- type: {type(page)}")
+        print(f"\t- content: {page}")
+        print(f"\t- utf8 content: {page.decode('utf-8')}")
