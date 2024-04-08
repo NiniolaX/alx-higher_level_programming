fb30c59d4990213b7e240b62d5c5f41ac14a520a
NiniolaX <loveafinni16@gmail.com>
2024-04-08 18:37:54 +0100

    Added documentation to module

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# Script fetches a URL
+"""
+Script fetches a URL https://alx-intranet.hbtn.io/status.
+"""
 
 if __name__ == '__main__':
     import urllib.request
-
     url = 'https://alx-intranet.hbtn.io/status'
-
     with urllib.request.urlopen(url) as response:
         page = response.read()
         print('Body response:')
