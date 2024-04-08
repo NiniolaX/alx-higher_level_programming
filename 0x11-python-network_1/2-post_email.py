3c9fab248657db6c91c8ef3910da9b719c655a4c
NiniolaX <loveafinni16@gmail.com>
2024-04-08 19:15:08 +0100

    Modified script: Decoded data before printinf response body

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -23,4 +23,4 @@ if __name__ == "__main__":
     # Send POST request to server
     with urllib.request.urlopen(req) as response:
         page = response.read()
-        print(page)
+        print(page.decode('utf-8'))
