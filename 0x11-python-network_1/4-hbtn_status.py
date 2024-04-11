b47a5217b0b9844ad500461c66ba3b63443d392a
root <loveafinni16@gmail.com>
2024-04-11 07:36:02 -0700

    Fixed pycodestyle errors

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,13 +2,14 @@
 """
 This script fetches a URL using Python requests package.
 """
-
-
 import requests
+
+
 def fetchUrl(url):
     """ Fetches a given URL """
     r = requests.get(url)
     print(f"Body response:\n\t- type: {type(r.text)}\n\t- content: {r.text}")
+
 
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
