54cdb9562bbd2e38350771456f89725649e268c1
NiniolaX <loveafinni16@gmail.com>
2024-04-16 18:33:17 +0100

    Fixed semistandard warnings

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
-exports.nbOccurrences = function (list, searchElement) {
+exports.nbOccurences = function (list, searchElement) {
   // Returns the number of occurences of an element in a list
   let nb = 0;
-  for (i = 0; i < list.length; i++) {
+
+  for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
       nb++;
     }
   }
+
   return (nb);
 }
