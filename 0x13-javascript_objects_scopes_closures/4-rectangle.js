48bd6a62e5fc4971b02396fa9c4fea7f6ecc5b36
NiniolaX <loveafinni16@gmail.com>
2024-04-16 14:12:59 +0100

    Added rotate and double methods to Rectangle class

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      // create an empty object
+    }
+  }
+
+  print () {
+    // Prints the rectangle using character 'X'
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    // Exchanges the width and height of the rectangle
+    const prev_height = this.height;
+    this.height = this.width;
+    this.width = prev_height;
+  }
+
+  double () {
+    // Multiplies the width and height of the rectangle by 2
+    this.height = 2 * this.height;
+    this.width = 2 * this.width;
+  }
+}
+
+module.exports = Rectangle;
